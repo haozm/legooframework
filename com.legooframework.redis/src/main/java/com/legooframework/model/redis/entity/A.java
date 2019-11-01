1546ef6ef7c88c56fb569abb3aa6bff3cd47a1b0
haoxj <haoxj@csosm.com>
2019-11-01 16:55:16 +0800

    ASDADSADASD

---
+++ com.legooframework.redis/src/main/java/com/legooframework/model/redis/entity/A.java
@@ -0,0 +1,13 @@
+package com.legooframework.model.redis.entity;
+
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class A {
+
+    //RedisStandaloneConfiguration
+    // JedisConnectionFactory
+//    RedisTemplate
+
+}
