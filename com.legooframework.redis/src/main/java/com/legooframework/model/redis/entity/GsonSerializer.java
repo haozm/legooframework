9b0106a6a7808fd816d3a666c1cb9e4fa9f1a191
haoxj <haoxj@csosm.com>
2019-11-06 09:28:53 +0800

    ASD

---
+++ com.legooframework.redis/src/main/java/com/legooframework/model/redis/entity/GsonSerializer.java
@@ -0,0 +1,5 @@
+package com.legooframework.model.redis.entity;
+
+// 这是一个标记接口
+public interface GsonSerializer {
+}
--- com.legooframework.redis/src/main/java/com/legooframework/model/redis/entity/MultipleSerializer.java
+++ com.legooframework.redis/src/main/java/com/legooframework/model/redis/entity/MultipleSerializer.java
@@ -7,9 +7,16 @@ public class MultipleSerializer implements RedisSerializer<Object> {
 
     private RedisSerializer jsonRedisSerializer = RedisSerializer.json();
     private RedisSerializer stringRedisSerializer = RedisSerializer.string();
+    private RedisSerializer gsonRedisSerializer = new GsonRedisSerializer();
 
     @Override
     public byte[] serialize(Object source) throws SerializationException {
+        if (source == null) {
+            return new byte[0];
+        }
+        if (source instanceof GsonSerializer) {
+            return gsonRedisSerializer.serialize(source);
+        }
         return new byte[0];
     }
 
