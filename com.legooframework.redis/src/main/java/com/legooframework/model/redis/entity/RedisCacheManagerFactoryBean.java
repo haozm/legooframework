ade8e2db755d14ebfc65d0c64029bb297c0786e1
xiaojie_hao <xiaojie.hao@live.com>
2019-11-09 00:29:47 +0800

    ASDASD

--- com.legooframework.redis/src/main/java/com/legooframework/model/redis/entity/RedisCacheManagerFactoryBean.java
+++ com.legooframework.redis/src/main/java/com/legooframework/model/redis/entity/RedisCacheManagerFactoryBean.java
@@ -11,11 +11,18 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Map;
 
 public class RedisCacheManagerFactoryBean extends AbstractFactoryBean<RedisCacheManager> {
 
-    private final static String DEFAULTCONFIG_VALUE = "ttl=600,nullValue=false,prefix=null";
+    private final static String DEFAULTCONFIG_VALUE = "name=cacheName,ttl=600,nullValue=false,prefix=null";
+    private RedisSerializer<String> valueSerialization = RedisSerializer.string();
+    private RedisConnectionFactory redisConnectionFactory;
+
+    public RedisCacheManagerFactoryBean(RedisConnectionFactory redisConnectionFactory) {
+        this.redisConnectionFactory = redisConnectionFactory;
+    }
 
     @Override
     public Class<RedisCacheManager> getObjectType() {
@@ -29,21 +36,30 @@ public class RedisCacheManagerFactoryBean extends AbstractFactoryBean<RedisCache
         String _defaultConfig = Strings.isNullOrEmpty(defaultConfig) ? DEFAULTCONFIG_VALUE : defaultConfig;
         Map<String, String> values = Splitter.on(',').withKeyValueSeparator('=').split(_defaultConfig);
         long ttl = MapUtils.getLongValue(values, "ttl", 0);
-        boolean nullValue = MapUtils.getBooleanValue(values, "nullValue", true);
-        String preifx = MapUtils.getString(values, "prefix", null);
         RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(ttl == 0L ? Duration.ZERO : Duration.ofSeconds(ttl))
-                .disableCachingNullValues().prefixKeysWith("hxj").serializeValuesWith(serializationPair);
+                .disableCachingNullValues().disableKeyPrefix().serializeValuesWith(serializationPair);
         return RedisCacheManager.RedisCacheManagerBuilder
                 .fromConnectionFactory(redisConnectionFactory)
                 .cacheDefaults(cacheConfig).transactionAware().build();
     }
 
-    private RedisConnectionFactory redisConnectionFactory;
-    private RedisSerializer<String> valueSerialization = RedisSerializer.string();
     private String defaultConfig;
+    private List<String> cacheConfigs;
 
-    public void setRedisConnectionFactory(RedisConnectionFactory redisConnectionFactory) {
-        this.redisConnectionFactory = redisConnectionFactory;
+    private RedisCacheConfiguration buildCacheConfig(String config) {
+        Map<String, String> values = Splitter.on(',').withKeyValueSeparator('=').split(config);
+        long ttl = MapUtils.getLongValue(values, "ttl", 0);
+        boolean nullValue = MapUtils.getBooleanValue(values, "nullValue", false);
+        String preifx = MapUtils.getString(values, "prefix", null);
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(ttl == 0L ? Duration.ZERO : Duration.ofSeconds(ttl));
+        if (!nullValue) cacheConfig.disableCachingNullValues();
+        if (Strings.isNullOrEmpty(preifx)) {
+            cacheConfig.disableKeyPrefix();
+        } else {
+            cacheConfig.prefixKeysWith(preifx);
+        }
     }
+
 }
