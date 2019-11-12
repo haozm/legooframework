fbf80abf160123a8dcf44679fadc0310399c2454
HaoXiaoJie <xiaojie.hao@live.com>
2019-11-12 13:55:51 +0800

    ASDASD

--- com.legooframework.smsresult/src/test/java/com/legooframework/model/smsresult/entity/ColumnMapStreamItemReaderTest.java
+++ com.legooframework.smsresult/src/test/java/com/legooframework/model/smsresult/entity/ColumnMapStreamItemReaderTest.java
@@ -4,7 +4,10 @@ import com.legooframework.model.core.jdbc.sqlengine.ColumnMapStreamItemReader;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.ResourceUtils;
@@ -18,19 +21,31 @@ import java.util.Map;
                 ResourceUtils.CLASSPATH_URL_PREFIX + "META-INF/entities/spring-model-cfg.xml",
                 ResourceUtils.CLASSPATH_URL_PREFIX + "META-INF/smsresult/spring-model-cfg.xml"}
 )
-public class ColumnMapStreamItemReaderTest {
+public class ColumnMapStreamItemReaderTest implements ApplicationContextAware {
 
     @Test
     public void doreazd() throws Exception {
-        Map<String, Object> item;
+        ItemReader<Map<String, Object>> reader = app.getBean("sms2HourSyncStreamItemReader", ColumnMapStreamItemReader.class);
+        Map<String, Object> item = null;
         for (; ; ) {
             item = reader.read();
             System.out.println(item);
-            if (item == null) return;
+            if (item == null) break;
+        }
+        ItemReader<Map<String, Object>> reader02 = app.getBean("sms2HourSyncStreamItemReader", ColumnMapStreamItemReader.class);
+        item = null;
+        for (; ; ) {
+            item = reader02.read();
+            System.out.println(item);
+            if (item == null) break;
         }
 
     }
 
-    @Autowired
-    private ColumnMapStreamItemReader reader;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.app = applicationContext;
+    }
+
+    ApplicationContext app;
 }
