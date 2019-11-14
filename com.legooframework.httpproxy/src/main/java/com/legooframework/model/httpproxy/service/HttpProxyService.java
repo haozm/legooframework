4e44e28cec9cbc4bfc50425e5fd31c9e289a6a24
HaoXiaoJie <xiaojie.hao@live.com>
2019-11-14 14:14:20 +0800

    ASDASD

--- com.legooframework.httpproxy/src/main/java/com/legooframework/model/httpproxy/service/HttpProxyService.java
+++ com.legooframework.httpproxy/src/main/java/com/legooframework/model/httpproxy/service/HttpProxyService.java
@@ -3,6 +3,7 @@ package com.legooframework.model.httpproxy.service;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 
@@ -15,12 +16,12 @@ public class HttpProxyService extends BundleService {
     /**
      * 接受
      *
-     * @param payload OXXO
+     * @param message OXXO
      * @return OOXX
      */
-    public Map<String, Object> postProxy(@Header Object haeder, @Payload Object payload) {
+    public Map<String, Object> postProxy(Message<?> message) {
         if (logger.isDebugEnabled())
-            logger.debug(String.format("[POST-PROXY] :%s , %s", haeder, payload));
+            logger.debug(String.format("[POST-PROXY] : %s", message));
         Map<String, Object> params = Maps.newHashMap();
         params.put("hao", "xiaojie");
         params.put("xiaojie", "hao");
